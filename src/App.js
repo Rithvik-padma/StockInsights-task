52ffc98abead02826c70a51b30fd7520b19498c8
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-04-27 21:26:37 +0530

    Added the time dropdown

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className='mt-20 flex flex-col justify-center items-start gap-2'>
         <h1 className='text-2xl font-bold'>Welcome to EquityGPT!</h1>
         <p className='text-md font-medium'>You can now ask questions to search through the Earnings Calls Transcripts!</p>
-        <div className='flex flex-row justify-start items-center gap-1'>
+        <div className='flex flex-row justify-start items-center gap-16'>
           <span className='text-lg '>Filters:</span>
           <SelectTime />
           <SelectStocks />
--- src/components/SelectTime.jsx
+++ src/components/SelectTime.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
 
 const SelectTime = () => {
+  const [toggleTime, setToggleTime] = useState(false)
+  const [checked, setChecked] = useState([]);
+
+  const timeList = ["Q2 FY23", "Q1 FY23", "Q4 FY22", "Q3 FY22", "Q2 FY22", "Q1 FY22"]
+
+  const toggleTimeDropdown =()=>setToggleTime(!toggleTime)  
+  const toggleSelectTime = (time) => {
+    let newChecked = [...checked]
+    if(checked.indexOf(time) === -1){
+      newChecked.push(time)
+    }
+    else{
+      newChecked = newChecked.filter((value) => value !== time)
+    }
+    setChecked(newChecked)
+  }
+
   return (
-    <div>
-      
+    <div className='relative'>
+      <button className={`w-40 my-2 bg-gray-200 text-gray-600 font-semimedium p-1 pl-0 rounded-md flex flex-row justify-between items-center ${toggleTime && "border border-solid border-gray-400"}`} onClick={toggleTimeDropdown}>
+        <span className='px-3 font-medium'>{checked.length > 0 ? "Time selected" : "Select Time" }</span>
+        {toggleTime ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+      </button>
+      {toggleTime &&
+        (<List className='!absolute rounded-sm bg-white w-full top-11 shadow-md'>
+          {timeList.map((time, index) => {
+          return(
+          <ListItem key={index} disablePadding>
+            <ListItemButton role={undefined} onClick={() => toggleSelectTime(time)} dense>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.indexOf(time) !== -1}
+                  tabIndex={-1}
+                />
+              </ListItemIcon>
+              <ListItemText id={index} primary={time} />
+            </ListItemButton>
+          </ListItem>
+          )})}
+        </List>)
+      }
     </div>
   )
 }
